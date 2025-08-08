f5bce068bb8e7db24fcee82142ab2c8df50ba425
Mounes14 <mounesneama@gmail.com>
2025-08-09 01:26:07 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('maryam-snake-v2').then(cache => cache.addAll(['./','./index.html','./manifest.json','./sw.js'])));
-});
-self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request))); });
